6a923fb119f1e588f89fec969e6bea5bbe65f6db
John <john@epicodus.com>
2017-10-16 10:34:14 -0700

    Add a test

---
+++ ToDoList.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+
+namespace ToDoList.Tests
+{
+    [TestClass]
+    public class ItemsTests
+    {
+		[TestMethod]
+		public void GetDescriptionTest()
+		{
+			//Arrange
+			var item = new Item();
+			item.Description = "Wash the dog";
+			//Act
+			var result = item.Description;
+			//Assert
+			Assert.AreEqual("Wash the dog", result);
+		}
+
+    }
+}
