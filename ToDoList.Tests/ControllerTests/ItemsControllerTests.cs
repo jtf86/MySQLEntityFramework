5f680d8363de613d38f8096d733e28d0f7c02045
John <john@epicodus.com>
2017-10-17 08:42:50 -0700

    Add some tests

---
+++ ToDoList.Tests/ControllerTests/ItemsControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using ToDoList.Controllers;
+
+namespace ToDoList.Tests.ControllerTests
+{
+    [TestClass]
+    public class ItemsControllerTests
+    {
+        [TestMethod]
+        public void GetDescriptionTest()
+        {
+            //Arrange
+            ItemsController controller = new ItemsController();
+            //Act
+            var result = controller.Index();
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(ActionResult));
+        }
+
+        [TestMethod]
+        public void Get_ModelList_Index_Test()
+        {
+            //Arrange
+            ViewResult indexView = new ItemsController().Index() as ViewResult;
+
+            //Act
+            var result = indexView.ViewData.Model;
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(List<Item>));
+        }
+    }
+}
